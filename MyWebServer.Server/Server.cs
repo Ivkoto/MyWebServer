c35606c9fb317fe1bd4fbfd5761ba938576d87c9
Ivaylo Kostov <ikostov87@gmail.com>
2021-06-06 19:16:42 +0300

    added class library for all the functionality such as headers, http method, requests, response, status code and etc.

---
+++ MyWebServer.Server/Server.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Net;
+
+namespace MyWebServer.Server
+{
+    public class Server
+    {
+        private readonly IPAddress ipAddress;
+        private readonly int port;
+
+        public Server(string ipAddress, int port)
+        {
+            this.ipAddress = IPAddress.Parse(ipAddress); 
+            this.port = port;
+        }
+    }
+}
--- MyWebServer/Program.cs
+++ MyWebServer/Program.cs
@@ -1,12 +1,22 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
 
 namespace MyWebServer
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main()
         {
+            var ipAddress = IPAddress.Parse("127.0.0.1");
+            var port = 8080;
 
+            var serverListener = new TcpListener(ipAddress, port);
+
+            serverListener.Start();
+
+            var connection = await serverListener.AcceptSocketAsync();
         }
     }
 }
