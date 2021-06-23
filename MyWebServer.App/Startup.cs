67ae8591e0cda94fcb23c0b6c5566f77a31053b7
Ivaylo Kostov <ikostov87@gmail.com>
2021-06-23 15:43:40 +0300

    Major file replacement.

---
+++ MyWebServer.App/Startup.cs
@@ -0,0 +1,31 @@
+﻿    namespace MyWebServer.App
+{
+    using System.Threading.Tasks;
+    using MyWebServer;
+    using MyWebServer.Controllers;
+    using MyWebServer.App.Controllers;
+
+    public class Startup
+    {
+        public static async Task Main()
+            => await new HttpServer(routes => routes
+                .MapStaticFiles()
+                .MapGet<HomeController>("/", c => c.Index())
+                .MapGet<HomeController>("/ToCats", c => c.LocalRedirect())
+                .MapGet<HomeController>("/Softuni", c => c.ToSoftUni())
+                .MapGet<HomeController>("/Error", c => c.Error())
+                .MapGet<HomeController>("/StaticFiles", c => c.StaticFiles())
+                .MapGet<AnimalsController>("/Cats", c => c.Cats())
+                .MapGet<AnimalsController>("/Dogs", c => c.Dogs())
+                .MapGet<AnimalsController>("/Bunnies", c => c.Bunnies())
+                .MapGet<AnimalsController>("/Turtles", c => c.Turtles())
+                .MapGet<AccountController>("/Login", c => c.Login())
+                .MapGet<AccountController>("/Logout", c => c.Logout())
+                .MapGet<AccountController>("/Cookies", c => c.CookiesCheck())
+                .MapGet<AccountController>("/Session", c => c.SessionCheck())
+                .MapGet<AccountController>("/Authentication", c => c.AuthenticationCheck())
+                .MapGet<CatsController>("/Cats/Create", c => c.Create())
+                .MapPost<CatsController>("/Cats/Save", c => c.Save()))
+            .Start();
+    }
+}
--- MyWebServer.Server/Http/HttpContentType.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MyWebServer.Server.Http
-{
-    public class HttpContentType
-    {
-        public const string PlainText = "text/plain; charset=UTF-8";
-        public const string Html = "text/html; charset=UTF-8";
-        public const string FormUrlEncoded = "application/x-www-form-urlencoded";
-    }
-}
